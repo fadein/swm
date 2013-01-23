36d05f8dd141d079b9b43b795b2cb0e85c33c385
Erik Falor <ewfalor@gmail.com>
2013-01-23 09:49:32 -0700

    Port some C macros, remove unneeded MIN/MAX

--- swm.scm
+++ swm.scm
@@ -29,15 +29,20 @@
 (use miscmacros)
 
 ;  /* macros */
-;  #define BUTTONMASK              (ButtonPressMask|ButtonReleaseMask)
+(define-syntax BUTTONMASK
+  (syntax-rules () ((_) (bitwise-ior BUTTONPRESSMASK BUTTONRELEASEMASK))))
 ;  #define CLEANMASK(mask)         (mask & ~(numlockmask|LockMask) & (ShiftMask|ControlMask|Mod1Mask|Mod2Mask|Mod3Mask|Mod4Mask|Mod5Mask))
-;  #define INTERSECT(x,y,w,h,m)    (MAX(0, MIN((x)+(w),(m)->wx+(m)->ww) - MAX((x),(m)->wx)) \
-;                                 * MAX(0, MIN((y)+(h),(m)->wy+(m)->wh) - MAX((y),(m)->wy)))
+(define-syntax INTERSECT
+  (syntax-rules ()
+				((_ x y w h m)
+				 (* (max 0 (- (min (+ x w) (+ (Monitor-wx m) (Monitor-ww m)))
+							  (max x (Monitor-wx m))))
+					(max 0 (- (min (+ y h) (+ (Monitor-wy m) (Monitor-wh m)))
+							  (max y (Monitor-wy m))))))))
 ;  #define ISVISIBLE(C)            ((C->tags & C->mon->tagset[C->mon->seltags]))
 ;  #define LENGTH(X)               (sizeof X / sizeof X[0])
-;  #define MAX(A, B)               ((A) > (B) ? (A) : (B))
-;  #define MIN(A, B)               ((A) < (B) ? (A) : (B))
-;  #define MOUSEMASK               (BUTTONMASK|PointerMotionMask)
+(define-syntax MOUSEMASK
+  (syntax-rules () ((_) (bitwise-ior BUTTONMASK POINTERMOTIONMASK))))
 ;  #define WIDTH(X)                ((X)->w + 2 * (X)->bw)
 ;  #define HEIGHT(X)               ((X)->h + 2 * (X)->bw)
 ;  #define TAGMASK                 ((1 << LENGTH(tags)) - 1)
